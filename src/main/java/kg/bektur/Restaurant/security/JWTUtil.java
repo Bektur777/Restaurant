8512d2170095fcc0e9753d8fc1741293850ea8f4
Bektur777 <ubei003@mail.ru>
2023-02-19 20:35:44 +0600

    Add generationToken method

--- src/main/java/kg/bektur/Restaurant/security/JWTUtil.java
+++ src/main/java/kg/bektur/Restaurant/security/JWTUtil.java
@@ -1,5 +1,30 @@
 package kg.bektur.Restaurant.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Component
 public class JWTUtil {
+    @Value("${jwt_secret}")
+    private String secret;
+
+    public String generationToken(String username) {
+        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(60).toInstant());
+
+        return JWT.create()
+                .withSubject("User details")
+                .withClaim("username", username)
+                .withIssuedAt(new Date())
+                .withIssuer("Restaurant")
+                .withExpiresAt(expirationDate)
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+
 
 }
