fc75257926754b1323e81c9198e70c98c70c93f9
Bektur777 <ubei003@mail.ru>
2023-02-19 16:48:08 +0600

    Add person repository with method to find person by username

---
+++ src/main/java/kg/bektur/Restaurant/repositories/PeopleRepository.java
@@ -0,0 +1,14 @@
+package kg.bektur.Restaurant.repositories;
+
+import kg.bektur.Restaurant.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
