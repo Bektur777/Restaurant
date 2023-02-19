eeee650b3362ed7080d7122a5e90a800466a84e3
Bektur777 <ubei003@mail.ru>
2023-02-19 15:21:45 +0600

    Initial commit

---
+++ src/test/java/kg/bektur/Restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bektur.Restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
