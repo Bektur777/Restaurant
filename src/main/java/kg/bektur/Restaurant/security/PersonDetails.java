54594ed340ed3bf0b5beca45e3b5f73d5bf1aedc
Bektur777 <ubei003@mail.ru>
2023-02-19 16:44:40 +0600

    Add 'UserDetails' wrapper to person entity

---
+++ src/main/java/kg/bektur/Restaurant/security/PersonDetails.java
@@ -0,0 +1,53 @@
+package kg.bektur.Restaurant.security;
+
+import kg.bektur.Restaurant.models.Person;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(person.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
