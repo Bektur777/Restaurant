b9721204fad9bbee5bc404c9e2f9dd0340ff593c
Bektur777 <ubei003@mail.ru>
2023-02-19 20:50:06 +0600

    Add 'RegistrationService' with method to save person to the db

---
+++ src/main/java/kg/bektur/Restaurant/services/RegistrationService.java
@@ -0,0 +1,22 @@
+package kg.bektur.Restaurant.services;
+
+import kg.bektur.Restaurant.models.Person;
+import kg.bektur.Restaurant.repositories.PeopleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationService {
+
+    private final PeopleRepository peopleRepository;
+
+    public RegistrationService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Transactional
+    public void register(Person person) {
+        peopleRepository.save(person);
+    }
+
+}
