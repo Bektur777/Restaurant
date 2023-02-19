647f6b338ce9994a8ee2320b6b9df6a372a2b85a
Bektur777 <ubei003@mail.ru>
2023-02-19 16:55:19 +0600

    Add security config to the project

--- src/main/java/kg/bektur/Restaurant/config/SecurityConfig.java
+++ src/main/java/kg/bektur/Restaurant/config/SecurityConfig.java
@@ -1,4 +1,28 @@
 package kg.bektur.Restaurant.config;
 
+import kg.bektur.Restaurant.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.userDetailsService(personDetailsService);
+
+        return http.build();
+    }
+
 }
