9fd84f92cabdc9944cc1530c3cf74fdd4a1522fc
Bektur777 <ubei003@mail.ru>
2023-02-19 18:07:26 +0600

    Add 'ModelMapper' to the project

--- src/main/java/kg/bektur/Restaurant/RestaurantApplication.java
+++ src/main/java/kg/bektur/Restaurant/RestaurantApplication.java
@@ -1,13 +1,20 @@
 package kg.bektur.Restaurant;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestaurantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
