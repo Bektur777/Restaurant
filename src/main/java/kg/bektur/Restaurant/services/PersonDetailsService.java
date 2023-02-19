f332d9c0b119cf18b09fb5596ca1d947cdebde66
Bektur777 <ubei003@mail.ru>
2023-02-19 16:52:31 +0600

    Add 'PersonDetailsService' class with the implemented method 'loadUserByUsername'

---
+++ src/main/java/kg/bektur/Restaurant/services/PersonDetailsService.java
@@ -0,0 +1,32 @@
+package kg.bektur.Restaurant.services;
+
+import kg.bektur.Restaurant.models.Person;
+import kg.bektur.Restaurant.repositories.PeopleRepository;
+import kg.bektur.Restaurant.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(username);
+        if (person.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new PersonDetails(person.get());
+    }
+
+}
