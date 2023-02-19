d3f62bd15f4038aaf847ddeefe69f2582d5654dd
Bektur777 <ubei003@mail.ru>
2023-02-19 22:13:28 +0600

    Add filter for authentication user

--- src/main/java/kg/bektur/Restaurant/config/JWTFilter.java
+++ src/main/java/kg/bektur/Restaurant/config/JWTFilter.java
@@ -1,17 +1,60 @@
 package kg.bektur.Restaurant.config;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kg.bektur.Restaurant.security.JWTUtil;
+import kg.bektur.Restaurant.services.PersonDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JWTFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+    private final PersonDetailsService personDetailsService;
+
+    public JWTFilter(JWTUtil jwtUtil, PersonDetailsService personDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.personDetailsService = personDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && !authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
 
+            if (jwt.isBlank()) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "Invalid JWT Token in Bearer Header");
+            }
+
+            try {
+                String username = jwtUtil.validateTokenAndRetrieveClaim(jwt);
+                UserDetails userDetails = personDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails.getPassword(),
+                        userDetails.getAuthorities()
+                );
+
+                if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+
+            } catch (JWTVerificationException exception) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JWT Token");
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 
 }
