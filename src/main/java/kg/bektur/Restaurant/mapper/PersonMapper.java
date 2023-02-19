ff0f4e84000064eac8c542f41d1caa7d5e02836a
Bektur777 <ubei003@mail.ru>
2023-02-19 21:31:45 +0600

    Update 'PersonMapper' with simplify this class

--- src/main/java/kg/bektur/Restaurant/mapper/PersonMapper.java
+++ src/main/java/kg/bektur/Restaurant/mapper/PersonMapper.java
@@ -2,22 +2,14 @@ package kg.bektur.Restaurant.mapper;
 
 import kg.bektur.Restaurant.dto.PersonDto;
 import kg.bektur.Restaurant.models.Person;
-import kg.bektur.Restaurant.repositories.PeopleRepository;
-import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PersonMapper extends AbstractMapper<Person, PersonDto> {
-    public PersonMapper(ModelMapper mapper, Class<Person> entityClass, Class<PersonDto> dtoClass) {
-        super(mapper, entityClass, dtoClass);
-    }
-
-    @Override
-    public Person toEntity(PersonDto dto) {
-        return super.toEntity(dto);
-    }
-
-    @Override
-    public PersonDto toDto(Person entity) {
-        return super.toDto(entity);
+    @Autowired
+    PersonMapper() {
+        super(Person.class, PersonDto.class);
     }
 
 }
