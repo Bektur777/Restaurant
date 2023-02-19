d44bff77433a9437592685986d322fd973c43c66
Bektur777 <ubei003@mail.ru>
2023-02-19 20:45:12 +0600

    Add method to validate token

--- src/main/java/kg/bektur/Restaurant/security/JWTUtil.java
+++ src/main/java/kg/bektur/Restaurant/security/JWTUtil.java
@@ -1,7 +1,11 @@
 package kg.bektur.Restaurant.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -25,6 +29,14 @@ public class JWTUtil {
                 .sign(Algorithm.HMAC256(secret));
     }
 
+    public String validateTokenAndRetrieveClaim(String token) throws JWTVerificationException {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
+                .withSubject("User details")
+                .withIssuer("Restaurant")
+                .build();
 
+        DecodedJWT jwt = verifier.verify(token);
+        return jwt.getClaim("username").asString();
+    }
 
 }
