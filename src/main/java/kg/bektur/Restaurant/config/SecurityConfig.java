e49bdb8759f195a64fc3f6a188d129e82e06968c
Bektur777 <ubei003@mail.ru>
2023-02-19 23:37:21 +0600

    Update security configuration method

--- src/main/java/kg/bektur/Restaurant/config/SecurityConfig.java
+++ src/main/java/kg/bektur/Restaurant/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import kg.bektur.Restaurant.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,8 +41,9 @@ public class SecurityConfig {
                         .failureUrl("/auth/login?error"))
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/auth/login"))
-                .sessionManagement(session -> session
+                        .logoutSuccessUrl("/auth/login"));
+
+        http.sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
@@ -53,6 +56,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
 }
