22ca60fc92899db4ae3e3d7b2108f3e29ec29a47
Bektur777 <ubei003@mail.ru>
2023-02-19 21:32:40 +0600

    Add registration method with jwt-token

--- src/main/java/kg/bektur/Restaurant/controllers/AuthController.java
+++ src/main/java/kg/bektur/Restaurant/controllers/AuthController.java
@@ -1,21 +1,40 @@
 package kg.bektur.Restaurant.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import kg.bektur.Restaurant.dto.PersonDto;
+import kg.bektur.Restaurant.mapper.PersonMapper;
+import kg.bektur.Restaurant.models.Person;
+import kg.bektur.Restaurant.security.JWTUtil;
+import kg.bektur.Restaurant.services.RegistrationService;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("")
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    private final JWTUtil jwtUtil;
+    private final RegistrationService registrationService;
+    private final PersonMapper personMapper;
 
-    @GetMapping("/auth/login")
+    public AuthController(JWTUtil jwtUtil, RegistrationService registrationService, PersonMapper personMapper) {
+        this.jwtUtil = jwtUtil;
+        this.registrationService = registrationService;
+        this.personMapper = personMapper;
+    }
+
+    @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
 
-    @GetMapping("/logout")
-    public String logoutPage() {
-        return "logout";
+    @PostMapping("/registration")
+    public Map<String, String> registration(@RequestBody PersonDto personDto) {
+        Person person = personMapper.toEntity(personDto);
+
+        registrationService.register(person);
+
+        return Map.of("jwt-token", jwtUtil.generationToken(person.getUsername()));
     }
 
 }
