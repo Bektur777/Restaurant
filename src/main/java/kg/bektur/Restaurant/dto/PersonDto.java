f437d296c6111fe7c726e73e3fc3f27d226f1e55
Bektur777 <ubei003@mail.ru>
2023-02-19 18:50:35 +0600

    Add person dto

---
+++ src/main/java/kg/bektur/Restaurant/dto/PersonDto.java
@@ -0,0 +1,67 @@
+package kg.bektur.Restaurant.dto;
+
+public class PersonDto extends AbstractDto {
+    private int id;
+    private String username;
+    private String fullName;
+    private String address;
+    private String email;
+    private String number;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
