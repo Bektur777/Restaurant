ecc2f64c8f4b309e7cd838375b77f82a18142f0c
Bektur777 <ubei003@mail.ru>
2023-02-19 21:30:10 +0600

    Update constructor with autowired mapper

--- src/main/java/kg/bektur/Restaurant/mapper/AbstractMapper.java
+++ src/main/java/kg/bektur/Restaurant/mapper/AbstractMapper.java
@@ -3,16 +3,17 @@ package kg.bektur.Restaurant.mapper;
 import kg.bektur.Restaurant.dto.AbstractDto;
 import kg.bektur.Restaurant.models.AbstractEntity;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Objects;
 
 public class AbstractMapper<E extends AbstractEntity, D extends AbstractDto> implements Mapper<E, D> {
-    private final ModelMapper mapper;
+    @Autowired
+    private ModelMapper mapper;
     private final Class<E> entityClass;
     private final Class<D> dtoClass;
 
-    public AbstractMapper(ModelMapper mapper, Class<E> entityClass, Class<D> dtoClass) {
-        this.mapper = mapper;
+    AbstractMapper(Class<E> entityClass, Class<D> dtoClass) {
         this.entityClass = entityClass;
         this.dtoClass = dtoClass;
     }
