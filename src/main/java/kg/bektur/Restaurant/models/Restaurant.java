db7b6a9d57456b7ac8390d094c6fa08dc717b05e
Bektur777 <ubei003@mail.ru>
2023-02-21 23:36:49 +0600

    Add entity of restaurant to the project

---
+++ src/main/java/kg/bektur/Restaurant/models/Restaurant.java
@@ -0,0 +1,74 @@
+package kg.bektur.Restaurant.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Restaurant")
+public class Restaurant extends AbstractEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone")
+    private String phone;
+
+    public Restaurant() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+}
