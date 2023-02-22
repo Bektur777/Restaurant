e6a7c2e9b7a267777b4aae5618d83dfd533c10f4
Bektur777 <ubei003@mail.ru>
2023-02-22 11:01:51 +0600

    Add CRUD methods to manipulations with Restaurant

--- src/main/java/kg/bektur/Restaurant/services/AdminService.java
+++ src/main/java/kg/bektur/Restaurant/services/AdminService.java
@@ -3,20 +3,22 @@ package kg.bektur.Restaurant.services;
 import kg.bektur.Restaurant.models.Person;
 import kg.bektur.Restaurant.models.Restaurant;
 import kg.bektur.Restaurant.repositories.PeopleRepository;
+import kg.bektur.Restaurant.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminService {
     private final PeopleRepository peopleRepository;
-    private final Restaurant restaurant;
+    private final RestaurantRepository restaurantRepository;
 
     @Autowired
-    public AdminService(PeopleRepository peopleRepository, Restaurant restaurant) {
+    public AdminService(PeopleRepository peopleRepository, RestaurantRepository restaurantRepository) {
         this.peopleRepository = peopleRepository;
-        this.restaurant = restaurant;
+        this.restaurantRepository = restaurantRepository;
     }
 
     public List<Person> findAllUsers() {
@@ -30,6 +32,22 @@ public class AdminService {
     public void updateRole(Person person, String role) {
         person.setRole(role);
         peopleRepository.save(person);
+    }
+
+    public List<Restaurant> findAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    public Optional<Restaurant> findRestaurantById(int id) {
+        return restaurantRepository.findById(id);
+    }
+
+    public void saveRestaurant(Restaurant restaurant) {
+        restaurantRepository.save(restaurant);
+    }
+
+    public void deleteRestaurant(Restaurant restaurant) {
+        restaurantRepository.delete(restaurant);
     }
 
 }
