9e60000e8897da65015e43b310ee32e7f4287d73
Bektur777 <ubei003@mail.ru>
2023-02-22 10:50:00 +0600

    Add method for update role to user

--- src/main/java/kg/bektur/Restaurant/services/AdminService.java
+++ src/main/java/kg/bektur/Restaurant/services/AdminService.java
@@ -1,6 +1,7 @@
 package kg.bektur.Restaurant.services;
 
 import kg.bektur.Restaurant.models.Person;
+import kg.bektur.Restaurant.models.Restaurant;
 import kg.bektur.Restaurant.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,12 @@ import java.util.List;
 @Service
 public class AdminService {
     private final PeopleRepository peopleRepository;
+    private final Restaurant restaurant;
 
     @Autowired
-    public AdminService(PeopleRepository peopleRepository) {
+    public AdminService(PeopleRepository peopleRepository, Restaurant restaurant) {
         this.peopleRepository = peopleRepository;
+        this.restaurant = restaurant;
     }
 
     public List<Person> findAllUsers() {
@@ -22,6 +25,11 @@ public class AdminService {
 
     public void delete(Person person) {
         peopleRepository.delete(person);
+    }
+
+    public void updateRole(Person person, String role) {
+        person.setRole(role);
+        peopleRepository.save(person);
     }
 
 }
