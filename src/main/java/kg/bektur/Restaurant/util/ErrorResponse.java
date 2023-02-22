b32af51c877e5d93fd1d92d44cc8c744835e5d3a
Bektur777 <ubei003@mail.ru>
2023-02-22 12:03:14 +0600

    Add error response class to response exceptions

---
+++ src/main/java/kg/bektur/Restaurant/util/ErrorResponse.java
@@ -0,0 +1,28 @@
+package kg.bektur.Restaurant.util;
+
+public class ErrorResponse {
+    private String message;
+    private long timestamp;
+
+    public ErrorResponse(String message, long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
