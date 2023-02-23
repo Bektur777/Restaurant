ecf69bac6b951a2986226e78bb078e2a528c4e15
Bektur777 <ubei003@mail.ru>
2023-02-23 21:10:48 +0600

    Add customer controller

---
+++ src/main/java/kg/bektur/Restaurant/controllers/CustomerController.java
@@ -0,0 +1,12 @@
+package kg.bektur.Restaurant.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+
+
+}
