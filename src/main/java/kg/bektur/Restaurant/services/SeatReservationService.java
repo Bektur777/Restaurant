6812fbe281342a209d7e8b5c8b1858f67258b586
Bektur777 <ubei003@mail.ru>
2023-02-23 10:36:34 +0600

    Add CRUD operations to SeatReservationService

--- src/main/java/kg/bektur/Restaurant/services/SeatReservationService.java
+++ src/main/java/kg/bektur/Restaurant/services/SeatReservationService.java
@@ -1,7 +1,12 @@
 package kg.bektur.Restaurant.services;
 
+import kg.bektur.Restaurant.models.Restaurant;
+import kg.bektur.Restaurant.models.SeatReservation;
 import kg.bektur.Restaurant.repositories.SeatReservationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SeatReservationService {
@@ -9,6 +14,27 @@ public class SeatReservationService {
 
     public SeatReservationService(SeatReservationRepository seatReservationRepository) {
         this.seatReservationRepository = seatReservationRepository;
+    }
+
+    public List<SeatReservation> findAllRestaurants() {
+        return seatReservationRepository.findAll();
+    }
+
+    public Optional<SeatReservation> findRestaurantById(int id) {
+        return seatReservationRepository.findById(id);
+    }
+
+    public void saveRestaurant(SeatReservation seatReservation) {
+        seatReservationRepository.save(seatReservation);
+    }
+
+    public void updateRestaurant(SeatReservation seatReservation, int id) {
+        seatReservation.setId(id);
+        seatReservationRepository.save(seatReservation);
+    }
+
+    public void deleteRestaurant(int id) {
+        seatReservationRepository.deleteById(id);
     }
 
 }
