df45288d42891528c2931d72fbd775d032948c0e
Bektur777 <ubei003@mail.ru>
2023-02-26 15:30:11 +0600

    Add render test

--- src/main/java/kg/bektur/Restaurant/RestaurantApplication.java
+++ src/main/java/kg/bektur/Restaurant/RestaurantApplication.java
@@ -5,8 +5,11 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestaurantApplication {
 
 	public static void main(String[] args) {
@@ -21,6 +24,11 @@ public class RestaurantApplication {
 				.setFieldMatchingEnabled(true)
 				.setSkipNullEnabled(true);
 		return mapper;
+	}
+
+	@GetMapping("/test")
+	public String testRender() {
+		return "Success";
 	}
 
 }
